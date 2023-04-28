2d2e1fac3d43d6a437bd27ec80601eae9bed89a7
alekseik1 <1alekseik1@gmail.com>
2023-04-28 09:39:48 +0300

    build(script): run on change

--- run_onchange_before_00-install-prerequisites.sh
+++ run_onchange_before_00-install-prerequisites.sh
@@ -6,3 +6,6 @@ if ! command -v tmux >/dev/null; then
     sudo apt update
     sudo apt install -y git python-is-python3 tmux python3 python3-dev build-essential vim python3-virtualenv curl wget zsh
 fi
+if ! command -v pyenv >/dev/null; then
+    curl https://pyenv.run | bash
+fi
